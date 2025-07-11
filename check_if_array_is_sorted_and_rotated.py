6eedf90c373162ea14a40d80bb0dd29b7e2569c9
sravyavutukuri007 <152386848+sravyavutukuri007@users.noreply.github.com>
2025-07-11 07:33:32 +0530

    Day2

--- check_if_array_is_sorted_and_rotated.py
+++ check_if_array_is_sorted_and_rotated.py
@@ -1,2 +1,21 @@
-#Problem link:https://leetcode.com/problems/check-if-array-is-sorted-and-rotated/
+#Problem link 
+ https://leetcode.com/problems/check-if-array-is-sorted-and-rotated/
 
+#Problem Summary
+Given an array, determine if it was originally sorted in non-decreasing order, then rotated.
+
+class Solution:
+    def check(self, nums: List[int]) -> bool:
+        counter = 0  # Initialize counter to count how many times the order breaks
+        
+        # Loop through all elements in the array
+        for i in range(len(nums)):
+            # Compare current element with the next one (circular using %)
+            if nums[i] > nums[(i + 1) % len(nums)]:
+                counter += 1  # If current > next, it's a "drop", so increment counter
+        
+        # If there's more than one "drop", it's not a rotated sorted array
+        if counter > 1:
+            return False
+        else:
+            return True
